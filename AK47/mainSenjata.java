caa27b734349476221e0e40516d39de567a214e0
Rahmanyahya <rahmanyahya647@gmail.com>
2024-01-17 20:22:52 +0700

    modified:   AK47/mainSenjata.java modified:   AK47/senjata.java

--- AK47/mainSenjata.java
+++ AK47/mainSenjata.java
@@ -1,12 +1,69 @@
 package AK47;
 
+import java.util.Scanner;
+
 public class mainSenjata {
   public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
     senjata Tembak = new senjata();
+    Boolean kondisi = true;
 
-    Tembak.jumlahpeluru = 30;
+    
+   loop1 : while (kondisi) {
+      System.out.print("masukan nama senjata anda : ");
+      Tembak.namaSenjata = input.next();
+      Tembak.reload();
 
-    Tembak.reload();
-    Tembak.tembak();
+      loop2: while (true) {
+        if (Tembak.jumlahpeluru < 30) {
+          System.out.println("\n1. TEMBAK\n2. RELOAD\n3. GANTI SENJATA\n4.Akhiri sesi");
+          System.out.print("PILIHAN : ");
+          int pilihan = input.nextInt();
+
+          if (pilihan == 1)
+            Tembak.tembak();
+          ;
+
+          if (pilihan == 2)
+            Tembak.reload();
+          ;
+
+          if (pilihan == 3)
+            continue loop1;
+
+          if (pilihan == 4) System.exit(0);
+
+            if (pilihan > 4 || pilihan < 1) {
+              System.out.println("anda harus memasukan angka pada menu");
+              continue loop2;
+            }
+          } else {
+           loop3: while (true) {
+             System.out.println("\n1. TEMBAK\n2. GANTI SENJATA\n3.Akhiri sesi");
+             System.out.print("Pilihan : ");
+             int pilihan = input.nextInt();
+
+             if (pilihan == 1) {
+               Tembak.tembak();
+               continue loop2;
+             }
+             
+             if (pilihan == 2)
+               continue loop2;
+
+             if (pilihan == 3)
+               System.exit(0);
+
+             if (pilihan > 3 || pilihan < 1) {
+               System.out.println("anda hars memasukan angka pada menu");
+               continue loop3;
+             }
+           }
+        }
+      }
+    }
+    
+    
+     
   }
 }
--- AK47/senjata.java
+++ AK47/senjata.java
@@ -5,13 +5,11 @@ import java.util.concurrent.TimeUnit;
 class senjata {
   String namaSenjata;
   int jumlahpeluru;
-  
-  
 
   void tembak() {
     if (jumlahpeluru <= 0) {
       System.out.println("peluru kosong.....");
-     reload();
+      reload();
     } else {
       System.out.println("menembak peluru.....");
       jumlahpeluru--;
@@ -19,20 +17,20 @@ class senjata {
     }
   }
 
-
   void reload() {
     System.out.println("reloading....");
-    tunggu();
-  }
-  
-  private void tunggu () {
+
     try {
-      TimeUnit.SECONDS.sleep(5);
+
+      TimeUnit.SECONDS.sleep(2);
       jumlahpeluru = 30;
       System.out.println("Senjata siap di gunakan lagi");
+      System.out.println("Peluru : " + jumlahpeluru);
     } catch (InterruptedException e) {
+
       Thread.currentThread().interrupt();
-      
+
     }
   }
+
 }
